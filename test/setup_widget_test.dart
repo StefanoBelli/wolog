d30d24b2e18abcd9da530d9ca5d6083278c08f87
StefanoBelli <stefano9913@gmail.com>
2023-09-18 18:15:32 +0200

    tests

--- test/setup_widget_test.dart
+++ test/setup_widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:wolog/main.dart';
 import 'package:wolog/page/setup.dart';
 
 void main() {
@@ -9,13 +8,10 @@ void main() {
     databaseFactory = databaseFactoryFfi;
   });
 
-  testWidgets('test', (final tester) async {
-    await tester.pumpWidget(const WologApp());
-    expect(find.text('anything'), findsNothing);
-  });
-
-  testWidgets('test2', (final tester) async {
-    await tester.pumpWidget(const MaterialApp(home: NoDbFoundPage()));
+  testWidgets('Ensure all required elements are there', (final tester) async {
+    await tester.pumpWidget(const MaterialApp(home: NoDbFoundPage())); 
     expect(find.text('No database found'), findsOneWidget);
+    expect(find.text('Create new database'), findsOneWidget);
+    expect(find.text('Import existing database...'), findsOneWidget);
   });
 }
